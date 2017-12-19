155b2c55c6a8375b047a5137db647abfe47204e2
Lee Hicks <leehicks@dreamfactory.com>
2017-12-19 16:47:47 -0500

    cleanup

--- routes/routes.php
+++ routes/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 $graphiQLRoute = config('graphiql.routes', 'graphiql');
-$controller = '\DreamFactory\Core\GraphiQL\Http\Controllers\GraphiQLController';
+$controller = 'DreamFactory\Core\GraphiQL\Http\Controllers\GraphiQLController';
 Route::get($graphiQLRoute, ['as' => 'graphiql.graphiql', 'uses' => $controller.'@graphiql']);
